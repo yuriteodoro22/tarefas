3a4ba8a3d4a895d38bdf966e7b1cc49377f95095
yuriteodoro22 <yuriteodoro2207@gmail.com>
2025-03-10 13:01:42 -0300

    Add comments

--- src/pages/task/[id].tsx
+++ src/pages/task/[id].tsx
@@ -1,91 +1,161 @@
-import Head from 'next/head';
-import styles from './styles.module.css'
-import { GetServerSideProps } from 'next';
-import { db } from '../../services/firebaseconnection'
-import { doc, collection, query, where, getDoc } from 'firebase/firestore';
-import { Textarea } from '@/components/textarea';
+import { ChangeEvent, FormEvent, useState } from "react";
+import { useSession } from "next-auth/react";
+import Head from "next/head";
+import styles from "./styles.module.css";
+import { GetServerSideProps } from "next";
+import { db } from "../../services/firebaseconnection";
+import { doc, collection, query, where, getDoc, addDoc, getDocs } from "firebase/firestore";
+import { Textarea } from "@/components/textarea";
 
-interface TaskProps{
-    item: {
-        tarefa: string;
-        public: boolean;
-        created: string;
-        user: string;
-        taskId: string;
+interface TaskProps {
+  item: {
+    tarefa: string;
+    public: boolean;
+    created: string;
+    user: string;
+    taskId: string;
+  };
+  allComments: CommentProps[]
+}
 
+interface CommentProps{
+    id: string;
+    comment: string;
+    taskId: string;
+    user: string;
+    name: string;
+}
+
+export default function Task({ item , allComments }: TaskProps) {
+  const { data: session } = useSession();
+
+  const [input, setInput] = useState("");
+  const [comments, setComments] = useState<CommentProps[ ]>(allComments || [])
+
+  async function handleComment(event: FormEvent) {
+    event.preventDefault();
+
+    if(input === '') return;
+
+    if(!session?.user?.email || !session?.user.name ) return;
+
+    try{
+        const docRef = await addDoc(collection(db, "comments"), {
+            comment: input,
+            created: new Date(),
+            user: session?.user?.email,
+            name: session?.user.name,
+            taskId: item?.taskId
+        })
+
+        setInput("")
+    }catch(err){
+        console.log(err)
     }
+  }
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Detalhes da tarefa</title>
+      </Head>
+
+      <main className={styles.main}>
+        <h1>Tarefa</h1>
+        <article className={styles.task}>
+          <p>{item?.tarefa}</p>
+        </article>
+      </main>
+
+      <section className={styles.commentsContainer}>
+        <h2>Deixar comentário</h2>
+
+        <form onSubmit={handleComment}>
+          <Textarea
+            value={input}
+            onChange={(event: ChangeEvent<HTMLTextAreaElement>) =>
+              setInput(event.target.value)
+            }
+            placeholder="Digite seu comentário..."
+          />
+          <button disabled={!session?.user} className={styles.button}>
+            Enviar comentário
+          </button>
+        </form>
+      </section>
+
+      <section className={styles.commentsContainer}>
+        <h2>Todos comentários</h2>
+        {comments.length === 0 && (
+            <span>Nenhum comentário foi encontrado...</span>
+        )}
+
+        {comments.map((item) => (
+         <article key={item.id} className={styles.comment}>
+            <p>{item.comment}</p>
+         </article>
+        ))}
+      </section>
+    </div>
+  );
 }
-
-export default function Task( { item } : TaskProps ){
-    return(
-        <div className={styles.container}>
-            <Head>
-                <title>Detalhes da tarefa</title>
-            </Head>
-
-            <main className={styles.main}>
-                <h1>Tarefa</h1>
-                <article className={styles.task}>
-                    <p>{item?.tarefa}</p>
-                </article>
-            </main>
-
-            <section className={styles.commentsContainer}>
-                <h2>Deixar comentário</h2>
-
-                <form>
-                    <Textarea
-                    placeholder='Digite seu comentário...'
-                    />
-                    <button className={styles.button}>Enviar comentário</button>                 
-                </form>
-            </section>
-        </div>
-    )
-}
-
-
 
 export const getServerSideProps: GetServerSideProps = async ({ params }) => {
-const id = params?.id as string;
- 
-const docRef = doc(db, "tarefas", id)
+  const id = params?.id as string;
 
-const snapshot = await getDoc(docRef)
+  const docRef = doc(db, "tarefas", id);
 
-if(snapshot.data() === undefined){
-   return{
-    redirect: {
-        destination: '/',
+  const q = query(collection(db, "comments"), where("taskId", "==", id))
+
+  const snapshotComments = await getDocs(q)
+
+  let allComments : CommentProps[] = [];
+  snapshotComments.forEach((doc) => {
+    allComments.push({
+        id: doc.id,
+        comment: doc.data().comment,
+        user: doc.data().user,
+        name:doc.data().name,
+        taskId: doc.data().taskId
+    });
+  });
+
+  console.log(allComments)
+
+  const snapshot = await getDoc(docRef);
+
+  if (snapshot.data() === undefined) {
+    return {
+      redirect: {
+        destination: "/",
         permanent: false,
-    }
-   }
-}
+      },
+    };
+  }
 
-if(!snapshot.data()?.public){
-    return{
-        redirect: {
-            destination: '/',
-            permanent: false,
-        }
-       }
-}
+  if (!snapshot.data()?.public) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
 
-const mileseconds = snapshot.data()?.created?.seconds * 1000;
+  const mileseconds = snapshot.data()?.created?.seconds * 1000;
 
-const task = {
+  const task = {
     tarefa: snapshot.data()?.tarefa,
     public: snapshot.data()?.public,
     created: new Date(mileseconds).toLocaleDateString(),
     user: snapshot.data()?.user,
-    taskID: id,
-}
+    taskId: id,
+  };
 
-console.log(task)
-
-    return{
-        props: {
-            item: task
-        },
-    };
+  return {
+    props: {
+      item: task,
+      allComments: allComments,
+    },
+  };
 };
