3ed285edc88c6635a7d8dc92fffad1c7c37e99f7
yuriteodoro22 <yuriteodoro2207@gmail.com>
2025-03-12 15:55:07 -0300

    Finalizando projeto

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,17 @@ import Head from "next/head";
 import styles from '../styles/home.module.css'
 import Image from "next/image";
 import HeroImage from '../../public/assets/hero.png'
+import { GetStaticProps } from "next";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "@/services/firebaseconnection";
+
+interface HomeProps{
+  posts: number;
+  comments: number
+}
 
 
-export default function Home() {
+export default function Home({ posts, comments }: HomeProps) {
   return (
    <div className={styles.container}>
       <Head>
@@ -26,14 +34,34 @@ export default function Home() {
 
         <div className={styles.infoContent}>
           <section className={styles.box}>
-            <span>+12 posts</span>
+            <span>+{posts} posts</span>
           </section>
           <section className={styles.box}>
-            <span>+90 comentários</span>
+            <span>+{comments} comentários</span>
           </section>
         </div>
       </main>
     
       </div>
   );
+}
+
+
+export const getStaticProps: GetStaticProps = async () => {
+ //buscr do banco os numeros e mandar para o componente
+
+ const commentRef = collection(db, "comments")
+
+ const commentSnapshot = await getDocs(commentRef)
+
+ const postRef = collection(db,"tarefas")
+
+ const postSnapshot = await getDocs(postRef)
+  return {
+    props:{
+      posts: postSnapshot.size || 0,
+      comments: commentSnapshot.size || 0,
+    },
+    revalidate: 60, // revalidada a cada 60s. A cada 60s ira buscar no nosso banco de dados os valores e atualizar as variaveis  com eles
+  }
 }
